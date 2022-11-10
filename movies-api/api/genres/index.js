cbfbdc9e09fe1aa45899b3512b28f8a6c1c28169
Vitor Wogel <vitorandre26@gmail.com>
2022-11-10 12:19:43 +0000

    Exercise completed

---
+++ movies-api/api/genres/index.js
@@ -0,0 +1,10 @@
+import { genres } from "./genresData";
+import express from "express";
+
+const router = express.Router(); 
+
+router.get('/', (req, res) => {
+    res.json(genres);
+});
+
+export default router
