d282731cec30821299fea8add8e8ea437bcb0328
Vitor Wogel <vitorandre26@gmail.com>
2022-11-24 12:24:44 +0000

    Mongo lab Exercises

--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -1,5 +1,7 @@
 import userModel from '../api/users/userModel';
+import genreModel from '../api/genres/genresSchema';
 import users from './users';
+import genres from './genres';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -16,6 +18,18 @@ async function loadUsers() {
   }
 }
 
+async function loadGenres() {
+  console.log('load genres Data');
+  try {
+    await genreModel.deleteMany();
+    await genreModel.collection.insertMany(genres);
+    console.info(`${genres.length} genres were successfully stored.`);
+  } catch (err) {
+    console.error(`failed to Load genre Data: ${err}`);
+  }
+}
+
 if (process.env.SEED_DB) {
   loadUsers();
+  loadGenres()
 }
